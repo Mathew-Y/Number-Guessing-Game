adbac17f33872efea1934ab5cc9001aed79fe8ac
Mathew-Y <mathewyounan@gmail.com>
2022-04-03 13:35:17 -0400

    Updated Sign in Page

--- app/src/main/java/com/example/myapplicationnew/SignInPage.java
+++ app/src/main/java/com/example/myapplicationnew/SignInPage.java
@@ -2,8 +2,11 @@ package com.example.myapplicationnew;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,7 +22,9 @@ public class SignInPage extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button loginButton = (Button) findViewById(R.id.loginButton);
+        TextView showOrHidePassword = (TextView)findViewById(R.id.showOrHidePassword);
         TextView createAccount = (TextView)findViewById(R.id.createAccountText);
+        TextView forgotPassword = (TextView)findViewById(R.id.forgotPasswordText);
         EditText usernameBox = (EditText)findViewById(R.id.userNameBox);
         EditText passwordBox = (EditText)findViewById(R.id.passwordBox);
 
@@ -29,7 +34,8 @@ public class SignInPage extends AppCompatActivity {
             public void onClick(View view) {
                 startActivity(new Intent(SignInPage.this, LevelSelectionPage.class));
                 username = usernameBox.getText().toString();
-                password = passwordBox.getText().toString();
+                usernameBox.setText("");
+                passwordBox.setText("");
             }
         });
 
@@ -37,6 +43,28 @@ public class SignInPage extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(SignInPage.this, CreateAccountPage.class));
+            }
+        });
+
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignInPage.this, ForgotPasswordPage.class));
+            }
+        });
+
+        showOrHidePassword.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                if(showOrHidePassword.getText().toString().equals("SHOW")) {
+                    passwordBox.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    showOrHidePassword.setText("HIDE");
+                }
+                else {
+                    passwordBox.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    showOrHidePassword.setText("SHOW");
+                }
             }
         });
     }
