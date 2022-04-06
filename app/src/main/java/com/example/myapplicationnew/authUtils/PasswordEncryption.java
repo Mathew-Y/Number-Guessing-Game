8c68a755d2f6be14ddf73c6bb7e9bcedce38d613
Mathew-Y <mathewyounan@gmail.com>
2022-04-06 13:41:08 -0400

    Proper password hashing/handling implemented

--- app/src/main/java/com/example/myapplicationnew/authUtils/PasswordEncryption.java
+++ app/src/main/java/com/example/myapplicationnew/authUtils/PasswordEncryption.java
@@ -6,13 +6,6 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 
 public class PasswordEncryption {
-//    public static String encrypt(String password) {
-//        return Base64.encodeToString(password.getBytes(), Base64.DEFAULT);
-//    }
-//
-//    public static String decrypt(String hash) {
-//        return new String(Base64.decode(hash, Base64.DEFAULT));
-//    }
     public static byte[] encryptMD5(byte[] data) throws Exception {
         MessageDigest md5 = MessageDigest.getInstance("MD5");
         md5.update(data);
